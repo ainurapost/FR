1b63d5b1354d8049dd8751382cdb102513ec3b62
ainurapost <ainurapost@gmail.com>
2021-10-10 22:55:12 +0600

    initial commit

---
+++ polls/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+
+app_name = 'polls'
+
+urlpatterns =[
+    path('', index),
+    path('<int:question_id>/', detail, name ='detail'),
+    path('<int:question_id>/results', results, name ='results'),
+    path('<int:question_id>/vote', vote, name ='vote'),
+
+]
